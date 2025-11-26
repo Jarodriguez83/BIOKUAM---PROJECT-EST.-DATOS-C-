7c34d94573064b28d4e40fe7798b3d91ec9b205b
Jarodriguez83 <jarodriguez83@ucatolica.edu.co>
2025-11-26 10:20:08 -0500

    PROGRAMA INTEGRANDO GRAFOS

---
+++ PROG_GRAFOS_BIOKUAM.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <windows.h> //LIBRERÍA UTILIZADA PARA LAS TILDES
+using namespace std; 
+
+int main(){
+	SetConsoleOutputCP(CP_UTF8); // Función para el uso de tildes en C++
+	cout<<"Análisis"; 
+}
